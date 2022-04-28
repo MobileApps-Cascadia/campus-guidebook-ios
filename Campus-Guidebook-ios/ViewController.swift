67db6d7dadbbfc95ad9f8083e01867ad4eceb6c3
othman <othmandjuliarso@gmail.com>
2022-04-28 12:44:50 -0700

    adding tableview properties

--- Campus-Guidebook-ios/ViewController.swift
+++ Campus-Guidebook-ios/ViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var cardTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+        
     }
 
 
