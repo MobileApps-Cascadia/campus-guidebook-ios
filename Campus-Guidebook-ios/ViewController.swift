90b89eded1258dd8fc5892c7ee8c9ce30875669a
othman <othmandjuliarso@gmail.com>
2022-04-28 12:50:52 -0700

    adding properties and sample asset

--- Campus-Guidebook-ios/ViewController.swift
+++ Campus-Guidebook-ios/ViewController.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet weak var cardTableView: UITableView!
     
+    @IBOutlet weak var cardTableView: UITableView!
+
+    let pictures: [UIImage] = []
+    let titles: [String] = []
+    let descriptions: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
    
-        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
 
