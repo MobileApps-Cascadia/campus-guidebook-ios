3a4d95f950f23f676e08c1510dbf05d8fa78b827
isaak wheeler <77463529+isaakwheeler@users.noreply.github.com>
2022-05-02 17:09:15 -0700

    Started creating the dao's i have lots i want to experiment with but here is the start of one. i want to make a generic class that you can implement that will have add row, remove row, get row, etc. then all you need to do is define setup and table references.

--- Campus-Guidebook-ios/model/Club.swift
+++ Campus-Guidebook-ios/model/Club.swift
@@ -6,8 +6,39 @@
 //
 
 import Foundation
+import SQLite3
+
 class Club: Codable{
-    var id: Int
+    var TableName: String = "Club"//track the table in which the dao is refering to for genaric queries
+    var Tablecolumns: String = "Name, Discription"//track values in columns in the table for queries
     var Name: String
     var Description: String
-}
+    var db = DataBaseHelper()// get the database
+    
+    init(name: String?, description: String?){ //optional values the have a default of blank
+        Name = name ?? ""
+        Description = description ?? ""
+    }
+    
+    func addLine(){
+        let ValueString: String = "\(Name), \(Description)"
+        
+        let insertStatementString = "INSERT INTO \(TableName) (\(Tablecolumns)) VALUES (\(ValueString));"
+        
+        var insertStatement: OpaquePointer?
+        
+        if sqlite3_prepare_v2(db.db, insertStatementString, -1, &insertStatement, nil) ==
+              SQLITE_OK {
+            if sqlite3_step(insertStatement) == SQLITE_DONE {
+              print("\nSuccessfully inserted row.")
+            } else {
+              print("\nCould not insert row.")
+            }
+          } else {
+            print("\nINSERT statement is not prepared.")
+          }
+          // 5
+          sqlite3_finalize(insertStatement)
+        }
+    }
+
