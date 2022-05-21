814e504529cd3ddfca981693df12edd8bfe716d9
Editable TableView <isaak.wheeler@hotmail.com>
2022-05-21 10:30:45 -0700

    adding add and remove tests for all the tables

--- Campus-Guidebook-iosTests/Campus_Guidebook_iosTests.swift
+++ Campus-Guidebook-iosTests/Campus_Guidebook_iosTests.swift
@@ -11,9 +11,16 @@ import XCTest
 class Campus_Guidebook_iosTests: XCTestCase {
     let dbase: DataBaseHelper = DataBaseHelper()
     let conn: OpaquePointer = dbase.GetOpenDB()
+    var mClub
+    var mEvent
+    var mSustainability
+    var results = []
 
     override func setUp() {
          //link databse here
+    mEvent: Event = Event(name: "Event TestName", description: "Test Event description")
+    mClub: Club = Club(name: "Club TestName", description: "Test Club description")
+    mSustainability: Sustainability = Sustainability(name: "Sustainability TestName", description: "Test Sustainability description")
     }
 
     override func tearDown() {
@@ -22,19 +29,30 @@ class Campus_Guidebook_iosTests: XCTestCase {
         
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test3TableAdd() throws {
+        mEvent.addRow(db: conn)
+        results = mEvent.getRow(db: conn, Search: "Event TestName")
+        XCTAssertEqual(results[0][1], "Event TestName")
+        mClub.addRow(db: conn)
+        results = mClub.getRow(db: conn, Search: "Club TestName")
+        XCTAssertEqual(results[0][1], "Club TestName")
+        mSustainability.addRow(db: conn)
+        results = mSustainability.getRow(db: conn, Search: "Sustainability TestName")
+        XCTAssertEqual(results[0][1], "Sustainability TestName")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test3TableRemove() throws {
+        mEvent.addRow(db: conn)
+        mEvent.removeRowByID(db: conn, id: 1)
+        results = mEvent.getRow(db: conn, Search: "Event TestName")
+        XCTAssertEqual(results[0][0], 2)
+        mClub.addRow(db: conn)
+        mClub.removeRowByID(db: conn, id: 1)
+        results = mEvent.getRow(db: conn, Search: "Club TestName")
+        XCTAssertEqual(results[0][0], 2)
+        mSustainability.addRow(db: conn)
+        mSustainability.removeRowByID(db: conn, id: 1)
+        results = mEvent.getRow(db: conn, Search: "Sustainability TestName")
+        XCTAssertEqual(results[0][0], 2)
     }
-
 }
