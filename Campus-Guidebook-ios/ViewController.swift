52a71bd03c8ef3dec6b06a1817eb88209040b8dd
othman <othmandjuliarso@gmail.com>
2022-05-26 15:29:15 -0700

    removing tests from view controller

--- Campus-Guidebook-ios/ViewController.swift
+++ Campus-Guidebook-ios/ViewController.swift
@@ -10,10 +10,8 @@ import SQLite3
 import Foundation
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    //______________________________//
-    let dbase: DataBaseHelper = DataBaseHelper()
-    
-    //______________________________//
+  
+//    let dbTest: DataBaseTests = DataBaseTests()
     
     let mainNavigationCardNames = ["Events", "Sustainability", "Student Clubs", "Arc", "Library", "Kodiac Corner", "Food Trucks", "Campus Map", ]
     
@@ -21,80 +19,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //__________Club_Tests___________//
-        print("\n\n\nThis is the start of the Club demo tests.")
-        var mClub: Club = Club(name: "Club TestName", description: "Test description") //make the new database row
-        
-        dbase.addClubRow(Club: mClub) //add the database row to the table
-        dbase.addClubRow(Club: mClub) //add the database row to the table
-        dbase.addClubRow(Club: mClub) //add the database row to the table
-        
-        var array = dbase.getAllTableContents(tablename: "Club")//get all rows
-        for item in array{
-            print(item)
-        }
-        
-        dbase.removeRowByID(tableName: "Club", id: 2)//remove a row with the id of 2
-        
-        array = dbase.getAllTableContents(tablename: "Club")//get all rows. the row with the id of 2 should be missing
-        print(array)
-        
-        array = dbase.getRow(tableName: "Club", Search: "Club TestName")//search for an entry by string
-        print("\n")
-        print(array)
-        
-        
-        
-//        //__________Event_Tests___________//
-        print("\n\n\nThis is the start of the Event demo tests.")
-        var mEvent: Event = Event(name: "Event TestName", description: "Test description") //make the new database row
-        
-        dbase.addEventRow(Event: mEvent) //add the database row to the table
-        dbase.addEventRow(Event: mEvent) //add the database row to the table
-        dbase.addEventRow(Event: mEvent) //add the database row to the table
-        
-        array = dbase.getAllTableContents(tablename: "Event")//get all rows
-        for item in array{
-            print(item)
-        }
-        
-        dbase.removeRowByID(tableName: "Event", id: 2)//remove a row with the id of 2
-        
-        array = dbase.getAllTableContents(tablename: "Event")//get all rows. the row with the id of 2 should be missing
-        print(array)
-        
-
-        array = dbase.getRow(tableName: "Event", Search: "Event TestName")//search for an entry by string
-        print("\n")
-        print(array)
-        
-
-//        //__________Sustainability_Tests___________//
-        print("\n\n\nThis is the start of the Sustainability demo tests.")
-        var mSustainability: Sustainability = Sustainability(name: "Sustainability TestName", description: "Test description", image: "Cats. All the cats.") //make the new database row
-                
-                dbase.addSustainabilityRow(Sustainability: mSustainability) //add the database row to the table
-                dbase.addSustainabilityRow(Sustainability: mSustainability) //add the database row to the table
-                dbase.addSustainabilityRow(Sustainability: mSustainability) //add the database row to the table
-                
-                array = dbase.getAllTableContents(tablename: "Sustainability")//get all rows
-                for item in array{
-                    print(item)
-                }
-                
-                dbase.removeRowByID(tableName: "Sustainability", id: 2)//remove a row with the id of 2
-                
-                array = dbase.getAllTableContents(tablename: "Sustainability")//get all rows. the row with the id of 2 should be missing
-                print(array)
-                
-                
-        array = dbase.getRow(tableName: "Sustainability", Search: "Sustainability TestName")//search for an entry by string
-        print("\n")
-        print(array)
-                
-
-//        //__________End_of_my_tests___________//
+        //do tests of db
+//        dbTest.removeTables()
+//        dbTest.makeTables()
+//        dbTest.makeTests()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
