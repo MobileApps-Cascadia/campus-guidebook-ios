9ad3220a60a80c3ccb73a3259087308bfcca8954
Haytham <haythamlion@outlook.com>
2022-05-26 18:05:37 -0700

    added sample data relevant to each category. added a checker to check if there is a url or not, and if there is an image or not and if there isn't it will choose a default image.

--- Campus-Guidebook-ios/model/helper/SampleData.swift
+++ Campus-Guidebook-ios/model/helper/SampleData.swift
@@ -25,24 +25,30 @@ class SampleData {
         "The engineering club is open to any student who is interested in science, technology, engineering, and math (STEM). Through hands on activities, members of all skill levels will have the opportunity to design, build, and share engineered projects with other creative problem solvers. Get ready to strengthen your skills, create a collection of projects related to your career, and connect with your peers! Some of the club projects we've undertaken include designing 3d printing models, making a video game with python, and electronic prototyping with Arduino.",
         "The purpose of this club is to provide a comfortable place for the students at Cascadia college to learn and experience Japanese culture together. In our club, we will share traditional Japanese culture such as Japanese calligraphy, origami, karate, etc. together."]
 
+    
     // Sample Data for events
     let eventPictures: [String] =
-    ["clubs_logo-1",
-     "dnd_club_logo",
-     "engineers_club",
-     "japanese_culture_club"]
-    let eventTitles: [String] = ["Clubs at Cascadia", "DnD Club", "Engineers Club", "Japanese Culture Club"]
-    let eventDescriptions: [String] = ["event", "event", "event", "event"]
+    ["https://padlet-uploads.storage.googleapis.com/621406566/f8a534f5b3cd80d89c87fcf7b2bc9392/Casino_Night_Final__11x17in_.png",
+     "https://www.cascadia.edu/images_calendar/collegerelations/CCEngineersClub.png",
+     "",
+     "",
+     ""]
+    let eventTitles: [String] = ["Events and Advocacy Board", "Engineering Club - Symposium", "Math Club - Weekly Meeting", "Math Club - Weekly Meeting", "Science Club - Weekly Meeting"]
+    let eventDescriptions: [String] = ["Roll the dice at EAB and CEB's annual Casino Night!", "The biggest Engineering event of the year", "Meets every other Tuesday at 3:30", "Meets every other Tuesday at 3:30", "Meets every other Tuesday at 3:30"]
 
 
     // Sample Data for sustainability
     let sustainabilityPictures: [String] =
-    ["clubs_logo-1",
-     "dnd_club_logo",
-     "engineers_club",
-     "japanese_culture_club"]
-    let sustainabilityTitles: [String] = ["Clubs at Cascadia", "DnD Club", "Engineers Club", "Japanese Culture Club"]
-    let sustainabilityDescriptions: [String] = ["sustainabaility", "sustainabaility", "sustainabaility", "sustainabaility"]
+    ["sustainability_wetlands",
+     "sustainability_green_buidlings",
+     "sustainability_campus_grounds",
+     "sustainability_stormwater_management"]
+    let sustainabilityTitles: [String] = ["Wetlands", "Green Buildings", "Campus Grounds", "Stormwater Management"]
+    let sustainabilityDescriptions: [String] = [
+        "We protect and continue to restore a 58-acre wetland that is one of the biggest floodplain restoration projects completed in the Pacific Northwest in conjunction with UW Bothell. Cascadia classes use the wetland as a living laboratory to study water quality, botany, ecology and wildlife biology. Cascadia students have done wetland stormwater sampling!",
+        "Our Global Learning and the Arts building (CC3) achieved Leadership in Energy and Environmental Design (LEED) Platinum  standard for environmental sustainability and we produce clean, renewable energy via solar panels located on our parking garages and rooftops.",
+        "Our campus, shared with University of Washington, Bothell, is Certified Salmon Safe and we produce herbs, flowers, fruits and vegetables in our campus Food Forest and Campus Farm using organic practices. We also provide habitat for native pollinators!",
+        "Our Campus is a secondary permitee under the Western Washington Phase II Municipal Stormwater Plan.  We manage stormwater with rain gardens, green stormwater infrastructure, signage and education, and working with our 58-acre restored wetland management!  You can see many of our projects by visiting the campus, or photos on our social media!"]
     
 }
 
