3945a25010753b5343cd7bb6c74a17c9b2a9d5a9
Haytham <haythamlion@outlook.com>
2022-05-28 10:55:17 -0700

    removed unnecessary code

--- Campus-Guidebook-ios/CardViewController.swift
+++ Campus-Guidebook-ios/CardViewController.swift
@@ -141,15 +141,6 @@ class CardsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
-    func verifyUrl (urlString: String?) -> Bool {
-        if let urlString = urlString {
-            if let url = NSURL(string: urlString) {
-                return UIApplication.shared.canOpenURL(url as URL)
-            }
-        }
-        return false
-    }
-    
 }
 
 extension UIImage {
