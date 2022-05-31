a134c28450073ea0106a7f53b35511d9f22fe3c7
othman <othmandjuliarso@gmail.com>
2022-05-30 19:40:14 -0700

    adds card detail view controller files.  trying to solve merging from develop into this branch.

--- Campus-Guidebook-ios/CardDetailViewController.swift
+++ Campus-Guidebook-ios/CardDetailViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class CardDetailViewController: UIViewController {
     
-    
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -24,34 +23,34 @@ class CardDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Card details View is loaded")
-
+        
         switch categoryID {
-          
-            case 0:
-                array = dbase.getRowByID(tableName: "Event", id: Int(id)!)
-                print("Event ID")
-                
-            case 1:
-                array = dbase.getRowByID(tableName: "Sustainability", id: Int(id)!)
-                print("Sus ID")
-                
-            case 2:
-                array = dbase.getRowByID(tableName: "Club", id: Int(id)!)
-                print("Club ID")
-                
-            case 3:
-                array = dbase.getRowByID(tableName: "Club", id: Int(id)!)
-                print("Club ID")
-                
-            default:
-                print("default?")
-            }
             
-            print( "Category id is \(categoryID!)")
+        case 0:
+            array = dbase.getRowByID(tableName: "Event", id: Int(id)!)
+            print("Event ID")
             
-            titleLabel.text = (array[0][1]) as? String
-            descriptionLabel.text = (array[0][2]) as? String
-            imgView.image = UIImage(named: (array[0][3] as? String)!)
+        case 1:
+            array = dbase.getRowByID(tableName: "Sustainability", id: Int(id)!)
+            print("Sus ID")
             
+        case 2:
+            array = dbase.getRowByID(tableName: "Club", id: Int(id)!)
+            print("Club ID")
+            
+        case 3:
+            array = dbase.getRowByID(tableName: "Club", id: Int(id)!)
+            print("Club ID")
+            
+        default:
+            print("default")
+        }
+        
+        print( "Category id is \(categoryID!)")
+        
+        titleLabel.text = (array[0][1]) as? String
+        descriptionLabel.text = (array[0][2]) as? String
+        imgView.image = UIImage(named: (array[0][3] as? String)!)
+        
     }
 }
