c3b12e61c9b7aaefc81376c0f0545a557133440b
Othman Djuliarso <othmandjuliarso@gmail.com>
2022-10-16 15:37:45 -0700

    added date, time, location and contact columns to club model

--- Campus-Guidebook-ios/model/Club.swift
+++ Campus-Guidebook-ios/model/Club.swift
@@ -13,21 +13,28 @@ import SQLite3
 class Club: Codable{
     var TableName: String = "Club"//track the table in which the dao is refering to for genaric queries
     var TableColumns: String = "name, description, imageurl"//track values in columns in the table for queries
-    var InsertableValueCount: Int = 3 //change this when you add a new column
+    var InsertableValueCount: Int = 7 //change this when you add a new column
     var Name: String
     var Description: String
     var ImageURL: String// add stuff here when you add a new column
-    
-    
+    // Todo: add meeting startDate, startTime, Location, ContactURL
+    var StartDate: String
+    var StartTime: String
+    var Location: String
+    var ContactURL: String
     
     private enum CodingKeys: String, CodingKey {
-            case Name, Description, ImageURL
+            case Name, Description, ImageURL, StartDate, StartTime, Location, ContactURL
         }
     
-    init(name: String?, description: String?, imageURL: String?){ //optional values the have a default of blank //change this when you add a new column
+    init(name: String?, description: String?, imageURL: String?, startDate: String?, startTime: String?, location: String?, contactURL: String?){ //optional values the have a default of blank //change this when you add a new column
         Name = name ?? ""
         Description = description ?? ""
         ImageURL = imageURL ?? ""// add stuff here when you add a new column
+        StartDate = startDate ?? ""
+        StartTime = startTime ?? ""
+        Location = location ?? ""
+        ContactURL = contactURL ?? ""
     }
         
     
