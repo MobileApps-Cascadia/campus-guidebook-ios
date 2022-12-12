681b7fa5fa4903d7aa2a8411411ee78b8b0aef90
isaakwheeler <isaak.wheeler@hotmail.com>
2022-12-12 10:00:41 -0800

    Fix reverted cleanup tasks from last issue

--- Campus-Guidebook-ios/model/DAOs/Event.swift
+++ Campus-Guidebook-ios/model/DAOs/Event.swift
@@ -8,12 +8,12 @@
 import Foundation
 import SQLite3
 class Event: Codable{
-    var TableName: String = "Event"//track the table in which the dao is refering to for genaric queries
-    var TableColumns: String = "name, description, ImageURL, StartDate, StartTime, CreationDate, Location, ContactURL"//track values in columns in the table for queries
-    var InsertableValueCount: Int = 8 //change this when you add a new column //TODO: Check if this is required
+    var TableName: String = "Event"//Tracks the table in which the dao is refering to for genaric queries
+    var TableColumns: String = "name, description, ImageURL, StartDate, StartTime, CreationDate, Location, ContactURL"//Tracks columns in the table for queries
+    var InsertableValueCount: Int = 8 //Change this when you add a new column
     var Name: String
     var Description: String
-    var ImageURL: String// add stuff here when you add a new column
+    var ImageURL: String //Add variables here when you add a new column
     var StartDate: String
     var StartTime: String
     var CreationDate: String
@@ -26,10 +26,10 @@ class Event: Codable{
             case Name, Description, ImageURL, StartDate, StartTime, CreationDate, Location, ContactURL
         }
     
-    init(name: String?, description: String?, imageURL: String?, startDate: String?, startTime: String?, creationDate: String?, location: String?, contactURL: String?){ //optional values the have a default of blank //change this when you add a new column
+    init(name: String?, description: String?, imageURL: String?, startDate: String?, startTime: String?, creationDate: String?, location: String?, contactURL: String?){ //Optional values have a default of blank //Change this when you add a new column
         Name = name ?? ""
         Description = description ?? ""
-        ImageURL = imageURL ?? ""// add stuff here when you add a new column
+        ImageURL = imageURL ?? "" //Add variables here when you add a new column
         StartDate = startDate ?? ""
         StartTime = startTime ?? ""
         CreationDate = creationDate ?? ""
